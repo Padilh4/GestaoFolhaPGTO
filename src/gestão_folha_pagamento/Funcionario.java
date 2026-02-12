2b7d78a603a1999ef90c5d35d00299bfdead90b0
Arthur Padilha <arthurcpadilha@gmail.com>
2026-02-12 15:26:32 -0300

    Meu projeto Java do NetBeans

---
+++ src/gestão_folha_pagamento/Funcionario.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestão_folha_pagamento;
+
+/**
+ *
+ * @author ARTHURCARDOSOPADILHA
+ */
+public class Funcionario {
+    
+}
---
+++ src/gestão_folha_pagamento/Gestão_Folha_pagamento.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gestão_folha_pagamento;
+
+/**
+ *
+ * @author ARTHURCARDOSOPADILHA
+ */
+public class Gestão_Folha_pagamento {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
